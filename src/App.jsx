0fc5b7d82178e2be34b3361cada070014e9ade9b
Onkar Patil <onkarvgpatil1998@gmail.com>
2023-12-18 15:04:31 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
  
  
 
-import  Cards  from './components/Cards'
+ 
+import { Header } from './layout/Header'
 import { Homepage } from './pages/Homepage'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
  
 
   return (
     <>
+    <Header />
       <Homepage />
  
     </>
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,5 @@
 import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+ 
 import '../App.css'
 function Cards({cardData}) {
    
@@ -10,20 +10,17 @@ function Cards({cardData}) {
         <Card.Title><h2>{cardData.StartupName}</h2></Card.Title>
         <br></br>
         <Card.Text>
-          Year: {cardData.Date.split('/')[2]}
+          <p>Year: {cardData.Date.split('/')[2]}</p>
+          <br></br>
+          <p> City: {cardData.CityLocation}</p>
+          <br>
+          </br>
+          <p>Funding: {cardData.AmountInUSD ? <>${cardData.AmountInUSD}</>: 'No Record '}</p>
         </Card.Text>
       </Card.Body>
-      <ListGroup className="list-group-flush">
-        <p>City: {cardData.CityLocation}</p>
-        <br>
-        </br>
-        <hr>
-        </hr>
-        <p>Funding: {cardData.AmountInUSD ? <>${cardData.AmountInUSD}</>: 'No Record Available'}</p>
-      </ListGroup>
       <Card.Body>
         <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+       
       </Card.Body>
     </Card>
    </div>
